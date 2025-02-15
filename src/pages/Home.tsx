34788950f984217353196b0395862f84dcaade22
ossamaabdein <ossamaabdein@gmail.com>
2025-02-15 16:52:56 +0200

    handle homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
+import '../styles/homepage/Home.scss'
 import React from 'react'
+import { yammLogo } from '../utils/icons'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homepage'>
+      <div className='intro'>
+        <div className='logo'>
+          {yammLogo}
+        </div>
+        <h1>Welcome to Yamm Admin Panel</h1>
+        <p>Let's help our customers and make their shopping experience easier.</p>
+      </div>
+    </div>
   )
 }
 
